d35102887aee1031cbcd09e69af1af7b70c0069a
Aaditya Nalawade <aaditya1@stanford.edu>
2026-01-07 22:20:38 -0800

    remove neocognitron implementation, setup background.

--- models/neocognitron/layers/s_layer.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-from models.neocognitron.ops.activation import relu
-from models.neocognitron.ops.utils import as_batch
-
--- models/neocognitron/ops/activation.py
+++
@@ -1,6 +0,0 @@
-from ctypes import Array
-import numpy as np
-
-def relu(input: Array) -> Array:
-    return np.maximum(0.0, input)
-    
